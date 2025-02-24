bada6db01f9fca1676862b03f1f587c5fbcb1a0d
Abdallah-Issa Kass <issakass02@gmail.com>
2025-02-24 19:50:30 +0200

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+	let n;
+	if (typeof arg !== "number") {
+		n = parseInt(arg);
+	} else {
+		n = arg;
+	}
 
+	if (n < 0) return "OOPS";
+	if (n === 0) return 0;
+
+	let n1 = 0;
+	let n2 = 1;
+
+	for (let i = 0; i < arg; i++) {
+		let current = n1 + n2;
+		n1 = n2;
+		n2 = current;
+	}
+
+	return n1;
 };
 
 // Do not edit below this line
